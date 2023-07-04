6b2cd849df26c5a61a12610203859a05829efeae
WebGreenTea <52134599+WebGreenTea@users.noreply.github.com>
2023-07-04 22:14:57 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function hoverProject(ele){ 
+    ele.querySelector("span").setAttribute("style", "display: flex;");
+}
+
+function mouseLeave(ele){
+    ele.querySelector("span").setAttribute("style", "display: none;");
+}
